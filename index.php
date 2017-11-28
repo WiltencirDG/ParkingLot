3b42a68f134c628ba437109826f6facef023c7f7
WiltencirDG <wiltencir@gmail.com>
2017-11-28 11:44:52 -0200

    Final Version

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html lang='en'>
 <header>
     <title>Estacionamento</title>
+    <link rel="icon" href="favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/style.css">
